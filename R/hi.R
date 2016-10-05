1735d1cf17f16bc42d9bf9e7cc877788c8e731e6
mark14226 <mark14226@gmail.com>
2016-10-05 17:29:45 -0400

    check input to hi()

--- R/hi.R
+++ R/hi.R
@@ -2,18 +2,22 @@
 
 # Add documentation to function - Roxygen:
 #' This function is a conversation starter
-#' 
-#' This function generates some helpful text that can be used to start 
+#'
+#' This function generates some helpful text that can be used to start
 #' conversations in all kinds of awkward social situations.
-#' 
+#'
 #' @param who character(1) The name of the person you wish to start a conversation with.
-#' 
+#'
 #' @return character(1) A line of text to be used when starting conversations.
-#' 
+#'
 #' @examples hi("Martin Morgan")
-#' 
+#'
 #' @export
 hi <- function(who) {
+    stopifnot(is.character(who),
+              length(who) == 1,
+              !is.na(who)
+              )
     paste("Hello", shout(who), "you have", nchar(who), "letters in your name!")
 }
 
